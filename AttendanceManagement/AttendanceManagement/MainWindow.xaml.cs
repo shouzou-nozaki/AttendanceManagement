1895716c189a2648ec8f9c2342bcf6ffe72d654e
shouzou-nozaki <soro0530@icloud.com>
2024-10-16 21:42:57 +0900

    初回コミット

---
+++ AttendanceManagement/AttendanceManagement/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace AttendanceApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            // 出勤時間を表示
+            lblStartTime.Content = "出勤時間: " + DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void btnEnd_Click(object sender, RoutedEventArgs e)
+        {
+            // 退勤時間を表示
+            lblEndTime.Content = "退勤時間: " + DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+}
