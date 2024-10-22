5d690e6bf300ff0eb626cd2a60d0eb486a2ace8f
shouzou-nozaki <soro0530@icloud.com>
2024-10-22 17:05:57 +0900

    コミット忘れ分

---
+++ AttendanceManagement/AttendanceManagement/dao/AttendanceInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceManagement.dao
+{
+    public class AttendanceInfo
+    {
+        public string StartTime { get; set; } = ""; // 始業時間
+        public string EndTime   { get; set; } = ""; // 終業時間
+        public string WorkTime  { get; set; } = ""; // 勤務時間 
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public AttendanceInfo()
+        {
+            this.StartTime = StartTime;
+            this.EndTime = EndTime;
+            this.WorkTime = WorkTime;
+        }
+    }
+}
